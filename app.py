95132a521b065fa2ba94b6f15611345e7d8f44ae
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-07-11 19:21:24 +0700

    add graphql with ariadne

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from logging import debug
-from flask import Flask, render_template
-from flask_pymongo import PyMongo
 import os
+
+from flask import Flask, render_template,request, jsonify
+from flask_pymongo import PyMongo
+
+from ariadne import QueryType, graphql_sync, make_executable_schema
+from ariadne.constants import PLAYGROUND_HTML
+
 from dotenv import dotenv_values
 
 config = dict(dotenv_values('.env'))
@@ -33,25 +37,8 @@ def login_route():
 def main_dashboard_page():
     return render_template('dashboard.html')
 
-
-data=[
-    {
-        'name':'Audrin',
-        'place': 'kaka',
-        'mob': '7736'
-    },
-    {
-        'name': 'Stuvard',
-        'place': 'Goa',
-        'mob' : '546464'
-    }
-]
-
-
 static_markdowns_dir = __dirname__ + '/markdowns'
 static_markdowns = os.listdir(static_markdowns_dir)
-
-print(static_markdowns)
 
 for markdown in static_markdowns:
     if markdown.endswith('.md'):
@@ -65,6 +52,52 @@ for markdown in static_markdowns:
             def privacy_policy():
                 return render_template('static_page.html', title=file_name.replace('-', ' '), content=contents)
 
+
+type_defs = """
+    type Query {
+        hello: String!
+    }
+"""
+
+query = QueryType()
+
+
+@query.field("hello")
+def resolve_hello(_, info):
+    request = info.context
+    user_agent = request.headers.get("User-Agent", "Guest")
+    return "Hello, %s!" % user_agent
+
+
+schema = make_executable_schema(type_defs, query)
+
+
+@app.route("/graphql", methods=["GET"])
+def graphql_playground():
+    # On GET request serve GraphQL Playground
+    # You don't need to provide Playground if you don't want to
+    # but keep on mind this will not prohibit clients from
+    # exploring your API using desktop GraphQL Playground app.
+    return PLAYGROUND_HTML, 200
+
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    # GraphQL queries are always sent as POST
+    data = request.get_json()
+
+    # Note: Passing the request to the context is optional.
+    # In Flask, the current request is always accessible as flask.request
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+    
 if __name__ == "__main__":  
     app.run(debug=True)
     print("DEBUG MODE")
